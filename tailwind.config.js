4d22a1fe8bffeb93b7471b486153ea2ca3b34acd
Guillermo Royo <guillermo.royo.rivera@gmail.com>
2024-02-16 16:34:12 -0300

    add: home styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,39 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'sanFrancisco' : "url('../img/sanFrancisco.jpg')",
+        'bali' : "url('../img/bali.jpg')",
+        'chicago' : "url('../img/chicago.jpg')",
+        'europe' : "url('../img/europe.jpg')",
+        'iceland' : "url('../img/iceland.jpg')",
+        'LA' : "url('../img/LA.jpg')",
+        'miami' : "url('../img/miami.jpg')",
+        'new_york' : "url('../img/new_york.jpg')",
+        'norway' : "url('../img/norway.jpg')",
+        'sanFranciscoDesktop' : "url('../img/sanFranciscoDesktop.jpg')",
+        'seattle' : "url('../img/seattle.jpg')",
+        'switzerland' : "url('../img/switzerland.jpg')",
+        'sydney' : "url('../img/sydney.jpg')",
+        'yosemite' : "url('../img/yosemite.jpg')",
+      },
+      backgroundColor: theme => ({
+        ...theme('colors'),
+        'primary': '#CC2D4A',
+        'secundary': '#8FA206',
+        'terciary': '#64AEC9',
+      }),
+      textColor: {
+        'primary': '#CC2D4A',
+        'secundary': '#8FA206',
+        'terciary': '#64AEC9',
+      },
+      fontFamily: {
+        Montserrat: ['Montserrat', 'sans-serif']
+      }
+
+    },
   },
   plugins: [],
 }
